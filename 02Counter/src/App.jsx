69a58c669f356244e3e820b970a632855f2d481e
Nishant <nishantkarena10@gmail.com>
2025-03-18 10:52:45 +0530

    Learned about useState hook, how it makes UI updation simpler and created a simple counter

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+  
+  function addValue(){
+    if(counter<20){
+      setCounter(counter+1)
+    }
+  }
+  function subValue(){
+    if(counter>0){
+      setCounter(counter-1)
+    }
+  }
+
   return (
-    <>
-      
-    </>
+
+    <div id='mainDiv'>
+      <h1 id='counter'>Counter : <span id='counterValue'>{counter}</span></h1>
+      <button id='add' onClick={addValue}>+ {counter}</button>
+      <button id='sub' onClick={subValue}>- {counter}</button>
+    </div>
+
   )
 }
 
